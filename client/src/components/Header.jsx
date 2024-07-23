6e10377c008d969d432e98d444c663da29e0063a
Santhossh15 <santhossh15@gmail.com>
2024-07-23 12:00:56 +0530

    Added Profile Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,7 +33,11 @@ const Header = () => {
             </li>
           </Link>
           <Link to="/profile">
-            <li className="text-slate-700 hover:underline">Sign In</li>
+            {currentUser ? (
+              <img src={currentUser.avatar} alt="avatar" />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
